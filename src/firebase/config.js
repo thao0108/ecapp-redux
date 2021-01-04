171f8198bb6afa1f1f060a63899f11126f938957
thao <thaopaopao@gmail.com>
2021-01-04 19:35:08 +0900

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAv29ClXkfoddJGFWttEvG47i9XQea6aS4",
+    authDomain: "ecapp-redux-2e839.firebaseapp.com",
+    projectId: "ecapp-redux-2e839",
+    storageBucket: "ecapp-redux-2e839.appspot.com",
+    messagingSenderId: "554435568426",
+    appId: "1:554435568426:web:aa9973c34374954c2b3453",
+    measurementId: "G-QW05ZWYMEL"
+  }; 
