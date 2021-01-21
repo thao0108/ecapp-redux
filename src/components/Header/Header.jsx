f3c74bf3766434f33a99d100ad9102db068dd587
thao <thaopaopao@gmail.com>
2021-01-21 21:42:49 +0900

    メニューバー追加

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux' 
+import { makeStyles} from '@material-ui/core/styles'
+import {AppBar, Toolbar} from '@material-ui/core'
+import logo from '../../assets/img/icons/logo.png'
+import { getIsSignedIn } from '../../reducks/users/selectors'
+import { push } from 'connected-react-router'
+
+ const useStyles = makeStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuBar: {
+            backgroundColor: "#fff",
+            color: "#444"
+        },
+        toolBar: {
+            margin: '0 auto',
+            maxWidth: '1024',
+            width: '100%'
+        },
+        iconButtons: {
+            margin: '0 0 0 auto'
+        }
+})
+
+const Header = () => {
+    const classes = useStyles()
+    const selector = useSelector(state => state)
+    const isSignedIn = getIsSignedIn(selector)
+    const dispatch = useDispatch()
+
+    return(
+        <div className={classes.root}>
+            <AppBar position="fiexed" className={classes.menuBar}>
+                <Toolbar className={classes.toolBar}>
+                    <img 
+                        src={logo} alt="Torahack logo" width="120px"
+                        onClick={() => dispatch(push('/'))}
+                    />
+                </ Toolbar> 
+            </ AppBar>
+        </div>
+    )
+}
+export default Header;
