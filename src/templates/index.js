1db5417822f28146c7b643f174699ccf1b413438
thao <thaopaopao@gmail.com>
2021-01-28 11:43:22 +0900

    注文確認画面作成

--- src/templates/index.js
+++ src/templates/index.js
@@ -6,5 +6,7 @@ export {default as ProductEdit} from './ProductEdit'
 export {default as ProductList} from './ProductList'
 export {default as ProductDetail} from './ProductDetail'
 export {default as CartList} from './CartList'
+export {default as OrderConfirm} from './OrderConfirm'
+
 
 
