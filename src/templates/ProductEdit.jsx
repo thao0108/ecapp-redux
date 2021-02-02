30c06a374d22764875c4ee9daa867b86d71ba514
thao <thaopaopao@gmail.com>
2021-02-02 22:00:04 +0900

    categoriesをfirestoreで管理取得

--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -20,6 +20,7 @@ const ProductEdit = () => {
     const [name, setName] = useState(''),
           [description, setDescription] = useState(''),
           [category, setCategory] = useState(''),
+          [categories, setCategories] = useState([]),
           [gender, setGender] = useState(''),
           [images, setImages] = useState([]),
           [sizes, setSizes] = useState([]),
@@ -38,11 +39,6 @@ const ProductEdit = () => {
     }, [setPrice])
 
 
-    const categories = [
-        {id: "tops", name: "トップス"},
-        {id: "shirts", name: "シャツ"},
-        {id: "pants", name: "パンツ"},
-    ]
     const genders = [
         {id: "all", name: "すべて"},
         {id: "male", name: "メンズ"},
@@ -66,6 +62,23 @@ const ProductEdit = () => {
                 })
         }
     }, [id])
+
+    useEffect(() => {
+        db.collection('categories')
+         .orderBy('order', 'asc')
+         .get()
+         .then(snapshots => {
+             const list = [] 
+             snapshots.forEach(snapshot => {
+                 const data = snapshot .data()
+                 list.push({
+                     id: data.id,
+                     name: data.name
+                 })
+             })
+             setCategories(list)
+         })   
+    }, [])
 
 
     return(
