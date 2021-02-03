69428c7fc278226402258d903751c7d33fd79997
thao <thaopaopao@gmail.com>
2021-02-03 19:23:37 +0900

    Drawerにメニュー追加

--- src/components/Header/ClosableDrawer.jsx
+++ src/components/Header/ClosableDrawer.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react'
+import React, {useState, useCallback, useEffect} from 'react'
 import { useDispatch } from 'react-redux'
 import { push } from 'connected-react-router'
 import Divider from '@material-ui/core/Divider'
@@ -16,31 +16,31 @@ import PersonIcon from '@material-ui/icons/Person'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import { TextInput } from '../../components/UIkit'
 import { signOut } from '../../reducks/users/oprations'
-
+import {db} from '../../firebase'
 
 const useStyles = makeStyles((theme) => ({
- drawer: {
-     [theme.breakpoints.up('sm')]:{
-         flexShrink: 0,
-         width: 256,
-       } 
-     },
-     toolbar: theme.mixins.toolbar,
-     drawerPaper: {
-         width: 256
-     },
-     searchFeild: {
-         alingItem: 'conter',
-         display: 'flex',
-         margin: 32
-     }
- 
+    drawer: {
+        [theme.breakpoints.up('sm')]:{
+            flexShrink: 0,
+            width: 256,
+          } 
+        },
+        toolbar: theme.mixins.toolbar,
+        drawerPaper: {
+            width: 256
+        },
+        searchFeild: {
+            alignItems: 'center',
+            display: 'flex',
+            margin: 32
+        }
+    
 }))
 
 const ClosableDrawer = (props) => {
+    const {container} = props
     const classes = useStyles()
     const dispatch = useDispatch()
-    const {container} = props
     const [keyword, setKeyword] = useState("")
     const inputKeyword = useCallback((event) => {
         setKeyword(event.target.value)
@@ -59,10 +59,33 @@ const ClosableDrawer = (props) => {
         { func: selectMenu, label: "プロフィール", icon: <PersonIcon />, id: "profile", value: "/value/mypage" },
     ]
 
+    const [filters, setFilters] = useState([
+        { func: selectMenu, label: "すべて", id: "all", value: "/"},
+        { func: selectMenu, label: "メンズ", id: "male", value: "/?gender=male"},
+        { func: selectMenu, label: "レディース", id: "female", value: "/?gender=female"},
+    ])
 
-    return (
-        <nav className={classes.drawer}>
-            <Drawer
+      //categoriesを取得 List中 mapで回す
+        useEffect(() => {
+        db.collection('categories')
+            .orderBy('order', 'asc')
+            .get()
+            .then(snapshots => {
+                const list = []
+                snapshots.forEach(snapshot => {
+                    const category = snapshot.data()
+                    list.push({ func: selectMenu, label: category.name, id: category.id, value: `/?category=${category.id}`})
+                }) 
+                // gender(filter)とcategoriesをまとめて新しい配列(filter)を作成
+                setFilters(prevState => [...prevState, ...list])
+            }) 
+    }, []) 
+
+
+
+    return(
+            <nav className={classes.drawer}>
+                <Drawer
                 container={container}
                 variant="temporary"
                 anchor="right"
@@ -92,16 +115,27 @@ const ClosableDrawer = (props) => {
                         </ListItem>
                     ))}
                     <ListItem button key="logout" onClick={() => dispatch(signOut())}>
-                        <ListItemIcon>
+                    <ListItemIcon>
                             <ExitToAppIcon />
                         </ListItemIcon>
                         <ListItemText primary={"Logout"} />
                     </ListItem>    
                 </List>
+                <Divider/>
+                <List>
+                    {filters.map(filter => (
+                        <ListItem 
+                         button
+                         key={filter.id}
+                         onClick={(e) => filter.func(e, filter.value)}
+                        >
+                            <ListItemText primary={filter.label}/>
+                        </ListItem>
+                    ))}
+                </List>
             </div>
             </Drawer>
-        </nav>
+        </nav> 
     )
- 
 }
 export default ClosableDrawer
