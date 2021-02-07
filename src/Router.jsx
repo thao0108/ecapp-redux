2034f5981d79176b2410e1db41c3fea248d58a75
thao <thaopaopao@gmail.com>
2021-02-07 21:39:18 +0900

    取得したクエリをfetchProductに渡す

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,17 +6,17 @@ import Auth from './Auth'
 const Router = () => {
     return (
         <Switch>
-                <Route exact path={"/signup"} component={SignUp} />
-                <Route exact path={"/signin"} component={SignIn} />
+                <Route exact path="/signup" component={SignUp} />
+                <Route exact path="/signin" component={SignIn} />
                 <Route exact path={"/signin/reset"} component={Reset} />
             <Auth>
-                <Route exact path={"(/)?"} component={ProductList} />
-                <Route exact path={"/product/:id"} component={ProductDetail} />
-                <Route path={"/product/edit(/:id)?"} component={ProductEdit} />
+                <Route exact path="(/)?" component={ProductList} />
+                <Route exact path="/product/:id" component={ProductDetail} />
+                <Route path="/product/edit(/:id)?" component={ProductEdit} />
 
-                <Route path={"/cart"} component={CartList} />
-                <Route path={"/order/confirm"} component={OrderConfirm} />
-                <Route path={"/order/history"} component={OrderHistory}/>
+                <Route path="/cart" component={CartList} />
+                <Route path="/order/confirm" component={OrderConfirm} />
+                <Route path="/order/history" component={OrderHistory}/>
 
             </Auth>
         </Switch>
